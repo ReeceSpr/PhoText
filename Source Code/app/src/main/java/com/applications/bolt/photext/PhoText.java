ce82daec1bba951145d446bc023dee1926f6a2d4
Reece <reece.spragg@gmail.com>
2018-12-04 21:26:05 +1300

    Camera Input and Vision Implemented, needs work on design front

--- Source Code/app/src/main/java/com/applications/bolt/photext/PhoText.java
+++ Source Code/app/src/main/java/com/applications/bolt/photext/PhoText.java
@@ -1,6 +1,7 @@
 package com.applications.bolt.photext;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.camera2.CameraAccessException;
@@ -9,20 +10,58 @@ import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telecom.Call;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.Window;
 import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
 import java.lang.Object.*;
+
 import static android.support.v4.app.ActivityCompat.requestPermissions;
 import static android.support.v4.content.ContextCompat.checkSelfPermission;
 
 public class PhoText extends AppCompatActivity {
 
     private TextView mTextMessage;
+    SurfaceView cameraView;
+    TextView textView;
+    CameraSource cameraSource;
+    final int RequestCameraPermissionID = 1001;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+            switch (requestCode)
+            {
+                case RequestCameraPermissionID:
+                {
+                    if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                        {
+                        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                            return;
+                        }
+                        try {
+                            cameraSource.start(cameraView.getHolder());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        }
+                }
+            }
+    }
+
+        private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
@@ -49,37 +88,71 @@ public class PhoText extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        getCamera();
 
+        cameraView = findViewById(R.id.surface_view);
+        textView = findViewById(R.id.text_view);
 
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        if (!textRecognizer.isOperational()) {
+            Log.w("MainActivity", "Dependencies are not available");
 
+        } else {
+            cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setRequestedFps(2.0f)
+                    .setAutoFocusEnabled(true)
+                    .build();
+            cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+                @Override
+                public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(PhoText.this, new String[]{Manifest.permission.CAMERA}, RequestCameraPermissionID);
+                        return;
+                    }
+                    try {
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                    cameraSource.stop();
+                }
+            });
+            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+                @Override
+                public void release() {
+
+                }
+
+                @Override
+                public void receiveDetections(Detector.Detections<TextBlock> detections) {
+
+                    final SparseArray<TextBlock> items = detections.getDetectedItems();
+                    if (items.size() != 0) {
+                        textView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                StringBuilder stringBuilder = new StringBuilder();
+                                for (int i = 0; i < items.size(); i++) {
+                                    TextBlock item = items.valueAt(i);
+                                    stringBuilder.append(item.getValue());
+                                    stringBuilder.append("\n");
+                                }
+                                textView.setText(stringBuilder.toString());
+                            }
+                        });
+                    }
+                }
+            });
+        }
     }
-    /*
-      Local Camera Import for imageFrame
-       */
-    void getCamera() {
-        String[] cameraIdList={"Null"};
-        Context context = getApplicationContext();
-        CallBackCamera callback = new CallBackCamera();
-        android.os.Handler handlerCamera = new android.os.Handler();
-        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
-            //Add Friendlyness
-            requestPermissions(new String[]{Manifest.permission.CAMERA}, 1888);
-        }
-        CameraManager cam = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
-        try {
-                cameraIdList = cam.getCameraIdList();
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-        for(String i:cameraIdList){
-            System.out.println(i);
-        }
-        try {
-            cam.openCamera(cameraIdList[0],callback,handlerCamera);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
 }
