59ba1a00857d88ee8a6b80ab4e92909542e956e1
Reece <reece.spragg@gmail.com>
2019-01-01 11:59:08 +1300

    Main OCR implemtned, part way through deisgn aspects.

--- Source Code/app/src/main/java/com/applications/bolt/photext/PhoText.java
+++ Source Code/app/src/main/java/com/applications/bolt/photext/PhoText.java
@@ -2,6 +2,8 @@ package com.applications.bolt.photext;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.camera2.CameraAccessException;
@@ -18,7 +20,9 @@ import android.util.SparseArray;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.vision.CameraSource;
@@ -32,13 +36,16 @@ import java.lang.Object.*;
 import static android.support.v4.app.ActivityCompat.requestPermissions;
 import static android.support.v4.content.ContextCompat.checkSelfPermission;
 
-public class PhoText extends AppCompatActivity {
+public class PhoText extends AppCompatActivity implements View.OnClickListener {
 
     private TextView mTextMessage;
     SurfaceView cameraView;
     TextView textView;
     CameraSource cameraSource;
     final int RequestCameraPermissionID = 1001;
+    int counter=0;
+    String rawString;
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -67,14 +74,14 @@ public class PhoText extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_previous:
+                    mTextMessage.setText(R.string.title_previous);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_capture:
+                    mTextMessage.setText(R.string.title_capture);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_help:
+                    mTextMessage.setText(R.string.title_help);
                     return true;
             }
             return false;
@@ -88,7 +95,54 @@ public class PhoText extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        getCamera(savedInstanceState);
+        addButtonListeners();
+    }
 
+    public void addButtonListeners(){
+        Button num = (Button)findViewById(R.id.buttonNum);
+        Button all = (Button)findViewById(R.id.buttonAll);
+        Button text = (Button)findViewById(R.id.buttonText);
+        num.setOnClickListener(this);
+        all.setOnClickListener(this);
+        text.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        String output = "";
+        switch (v.getId()) {
+            case  R.id.buttonNum: {
+                if(rawString!=null){
+                    output = filterNum();
+                    ClipData clip = ClipData.newPlainText("NUMBERS", output);
+                    ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                    clipboard.setText(output);
+                }
+                break;
+            }
+
+            case R.id.buttonAll: {
+                if(rawString!=null){
+                    output = rawString;
+                    ClipData clip = ClipData.newPlainText("NUMBERS", output);
+                    ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                    clipboard.setText(output);
+                }
+                break;
+            }
+            case R.id.buttonText: {
+                if(rawString!=null){
+                    output = filterText();
+                    ClipData clip = ClipData.newPlainText("NUMBERS", output);
+                    ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                    clipboard.setText(output);
+                }
+            }
+            //.... etc
+        }
+    }
+
+    public void getCamera(Bundle savedInstanceState) {
         cameraView = findViewById(R.id.surface_view);
         textView = findViewById(R.id.text_view);
 
@@ -99,8 +153,8 @@ public class PhoText extends AppCompatActivity {
         } else {
             cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
                     .setFacing(CameraSource.CAMERA_FACING_BACK)
-                    .setRequestedPreviewSize(1280, 1024)
-                    .setRequestedFps(2.0f)
+                    .setRequestedPreviewSize(720, 1280)
+                    .setRequestedFps(60)
                     .setAutoFocusEnabled(true)
                     .build();
             cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
@@ -135,24 +189,64 @@ public class PhoText extends AppCompatActivity {
 
                 @Override
                 public void receiveDetections(Detector.Detections<TextBlock> detections) {
-
                     final SparseArray<TextBlock> items = detections.getDetectedItems();
-                    if (items.size() != 0) {
-                        textView.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                StringBuilder stringBuilder = new StringBuilder();
-                                for (int i = 0; i < items.size(); i++) {
-                                    TextBlock item = items.valueAt(i);
-                                    stringBuilder.append(item.getValue());
-                                    stringBuilder.append("\n");
+                    if (counter < 4) {
+                        counter++;
+                    } else {
+                        counter = 0;
+                        if (items.size() != 0) {
+                            textView.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    StringBuilder stringBuilder = new StringBuilder();
+                                    for (int i = 0; (i < items.size()); i++) {
+                                        TextBlock item = items.valueAt(i);
+                                        stringBuilder.append(item.getValue());
+                                        stringBuilder.append(" ");
+                                    }
+                                    textView.setText(stringBuilder.toString());
+                                    rawString = stringBuilder.toString();
                                 }
-                                textView.setText(stringBuilder.toString());
-                            }
-                        });
+                            });
+                        }
                     }
                 }
             });
         }
     }
+
+    /*Filters the numbers from the strings
+    IMPLEMENTED
+    */
+    public String filterNum(){
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < rawString.length(); i++){
+            if (Character.isDigit(rawString.charAt(i))){
+                out.append(rawString.charAt(i));
+            }
+        }
+        return out.toString();
+        }
+
+    /*Filters the text from the strings
+     Would like spell check compare
+     */
+    public String filterText(){
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < rawString.length(); i++){
+            if (Character.isLetter(rawString.charAt(i))){
+                out.append(rawString.charAt(i));
+            }
+        }
+        return out.toString();
+    }
+
+
+    //Forced Declaration, unimplemented
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 }
+
+
